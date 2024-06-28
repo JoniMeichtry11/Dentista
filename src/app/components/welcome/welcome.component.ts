0a916c89450592f1b4d0c6b74ade93c4bca3783b
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-06-27 22:12:00 -0300

    feat: add first section in welcome section

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -6,8 +6,33 @@ import { SwiperCarouselComponent } from '../shared/swiper-carousel/swiper-carous
   standalone: true,
   imports: [SwiperCarouselComponent],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
 export class WelcomeComponent {
-
+  listData = [
+    {
+      title: 'LA TECNOLOGÍA MÁS AVANZADA',
+      description:
+        'En nuestra clínica dental utilizamos la última tecnología disponible en el mercado para que su experiencia sea siempre excepcional y consiga los resultados que busca.',
+      icon: 'icon-[streamline--ai-technology-spark-solid]',
+    },
+    {
+      title: 'PROFESIONALES ESPECIALIZADOS',
+      description:
+        'Nuestros dentistas y asistentes están 100% capacitados para brindarle el mejor servicio durante sus procedimientos.',
+      icon: 'icon-[vaadin--doctor]',
+    },
+    {
+      title: 'ATENCIÓN PERSONALIZADA',
+      description:
+        'Nuestro equipo le acompañará en cada etapa de su tratamiento dental, ofreciéndole soluciones basadas en su diagnóstico y necesidades específicas.',
+      icon: 'icon-[fluent--dentist-24-filled]',
+    },
+    {
+      title: 'PLANES ASEQUIBLES SIN SORPRESAS',
+      description:
+        'En nuestra clínica, encontrará planes dentales realmente asequibles sin sorpresas en los pagos y opciones de financiación',
+      icon: 'icon-[streamline--money-cash-file-dollar-common-money-currency-cash-file]',
+    },
+  ];
 }
